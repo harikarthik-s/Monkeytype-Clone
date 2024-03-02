e7ee4dd6e0d15efb7cf9615d11f7243e82df7e25
harikarthik-s <harikarthiks15@gmail.com>
2024-03-03 00:05:54 +0530

    theme added

---
+++ script.js
@@ -0,0 +1,27 @@
+const toggleModeInput = document.getElementById("light-mode-toggle");
+const logoImage = document.getElementById("logo");
+
+toggleModeInput.addEventListener("click", (e) => {
+  if (!document.body.classList.contains("light-mode")) {
+    document.body.classList.add("light-mode");
+    localStorage.setItem("lightMode", true);
+    logoImage.src = "assets/logo-light-mode.svg";
+  } else {
+    document.body.classList.remove("light-mode");
+    localStorage.setItem("lightMode", false);
+    logoImage.src = "assets/logo.svg";
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const lightMode = JSON.parse(localStorage.getItem("lightMode"));
+
+  if (!lightMode) {
+    localStorage.setItem("lightMode", false);
+    logoImage.src = "assets/logo.svg";
+  } else {
+    document.body.classList.add("light-mode");
+    toggleModeInput.checked = true;
+    logoImage.src = "assets/logo-light-mode.svg";
+  }
+});
